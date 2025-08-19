6dbe638d6fd61733aa40d405b391a2d15ad6e988
malikanish <anishbilal001@gmail.com>
2025-08-19 12:49:19 +0500

    abdu

--- userdata.sh
+++ userdata.sh
@@ -30,7 +30,7 @@ GH_PAT=$(aws ssm get-parameter --name "$SSM_PARAM_NAME" --with-decryption --regi
 
 # -------------------------------
 # Update ASG Desired Capacity (1 job = 1 insta
-# -------------------------------
+# ----------------------------
 ASG_NAME="final-asg"   # apna Auto Scaling Group ka naam d
 
 # Get current desired capacity
